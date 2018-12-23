7557895c994d12e38500fae6f79d7b81d1a2b8d2
vigilantPotato <vigilant.potato@gmail.com>
2018-12-23 15:35:14 +0900

    add show_weather

--- WeatherGetter.py
+++ WeatherGetter.py
@@ -36,8 +36,10 @@ class ShowWeather(tkinter.LabelFrame):
     '''
     def __init__(self, master=None):
         super().__init__(master)
-        self.label_list = []
+        self.create_widget()    #ラベル、ボタンウィジェットを生成
+        self.show_weather()     #天気予報を取得し、ラベルに表示
 
+    def create_widget(self):
         f1 = tkinter.Frame(self)
         l = tkinter.Label(master=f1, text='', width=5, relief=tkinter.RAISED)
         l.grid(row=0, column=0)
@@ -47,7 +49,6 @@ class ShowWeather(tkinter.LabelFrame):
         l.grid(row=0, column=2)
         l = tkinter.Label(master=f1, text='明後日', width=8, relief=tkinter.RAISED)
         l.grid(row=0, column=3)
-        f1.grid(row=0,column=0,columnspan=2)
 
         f2 = tkinter.Frame(self)
         l = tkinter.Label(master=f2, text='天気', width=5, relief=tkinter.RAISED)
@@ -56,23 +57,27 @@ class ShowWeather(tkinter.LabelFrame):
         l.grid(row=1, column=0)
         l = tkinter.Label(master=f2, text='min', width=5, relief=tkinter.RAISED)
         l.grid(row=2, column=0)
-        f2.grid(row=1,column=0)
 
         f3 = tkinter.Frame(self)
+        self.label_list = []
         for i in range(3):
             for j in range(3):
                 l = tkinter.Label(master=f3, width=8, relief=tkinter.RAISED)
                 l.grid(row=i, column=j)
                 self.label_list.append(l)
+        
+        #フレームの表示
+        f1.grid(row=0,column=0,columnspan=2)
+        f2.grid(row=1,column=0)
         f3.grid(row=1,column=1)
 
-        b = tkinter.Button(self, text='Reflesh', command=self.show)
+        b = tkinter.Button(self, text='Reflesh', command=self.show_weather)
         b.grid(row=2, columnspan=2, sticky=tkinter.E)
 
-    def show(self):
-        place = {'city' :'140010'}
-        self.w = GetWeather(place)
-        self.w.get_weather()
+    def show_weather(self):
+        place = {'city' :'140010'}  #横浜市
+        self.w = GetWeather(place)  #GetWeatherオブジェクト生成
+        self.w.get_weather()        #天気、気温情報を取得
 
         #天気予報表示
         for i, l in enumerate(self.w.weather):
@@ -84,6 +89,5 @@ if __name__ == "__main__":
     root = tkinter.Tk()
     root.title('WeatherGetter') 
     app = ShowWeather(master=root)
-    app.show()
     app.pack()
     root.mainloop()
